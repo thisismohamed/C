c82311d3622573b2d044666d59342b737de90688
المعلم محمد <itslambda3@gmail.com>
2024-11-07 20:16:58 +0200

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -19,20 +19,17 @@ int main() {
                         result = num1 - num2;
                         break;
                 case '*':
-                        result = num1 * num2;
+                        result = num1 *num2;
                         break;
                 case '/':
-                        if (num2 != 0) {
-                                result = num1 / num2;
-                        } else {
-                                printf("Cannot divide by zero");
-                        }
+                        result = num1 / num2;
                         break;
                 default:
-                        printf("Invalid an operation");
-                        return 1;
+                        printf("Invalid an operation\n");
+                        result = -DBL_MAX;
         }
 
-        printf("Result: %.2lf", result);
+        if (result != -DBL_MAX)
+                printf("Result: %.2lf", result);
         return 0;
 }
